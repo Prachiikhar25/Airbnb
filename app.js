dd0302d283dc8cfa009ef6507a5c148f03b5db86
Portal Creator <prachiikhar42@gmail.com>
2025-09-16 21:12:15 +0530

    Connected database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js")
 
 const MONGO_URL= "mongodb://127.0.0.1:27017/Airbnb";
 
@@ -19,6 +20,19 @@ async function main() {
 app.get("/",(req, res)=>{
 	res.send("Hi I am root.");
 })
+
+app.get("/testListing",async (req, res)=>{
+	let sampleListing= new Listing({
+		title:"My new Villa",
+		description:"By the beach",
+		price:1200,
+		location:"Calangute, Goa",
+		country:"India"
+	});
+	await sampleListing.save();
+	console.log("Sample was saved.");
+	res.send("successful testing");
+});
 
 app.listen(8080, ()=>{
 	console.log("srever is listening to port 8080.")
